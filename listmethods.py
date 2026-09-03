de54beba72f7f5a764fc550e969cf411a50dafa1
gopirajsathyashouri <gopirajsathyashouri@gmail.com>
2026-09-03 23:26:24 +0530

    Add list methods

---
+++ listmethods.py
@@ -0,0 +1,6 @@
+movie_collection = ["Avengers", "Iron Man", "Thor", "Captain America", "Hulk"]
+movie_collection.append("Black Panther")
+movie_collection.pop(3)
+movie_collection.reverse()
+movie_collection.insert(3, "Doctor Strange")
+print(movie_collection)
