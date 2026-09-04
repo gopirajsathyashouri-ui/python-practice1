168e33449b781919afd704de0b904ed6d574146c
gopirajsathyashouri <gopirajsathyashouri@gmail.com>
2026-09-04 19:23:38 +0530

    Add array

---
+++ #array.py
@@ -0,0 +1,11 @@
+#array.py
+
+my_array = [1, 2, 3, 4, 5, 6]
+first_element = my_array[0]
+print("First element:", first_element)
+
+my_array[2] = 7
+print("Updated array:", my_array)
+
+my_array.append(8)
+print("Array after appending:", my_array)
