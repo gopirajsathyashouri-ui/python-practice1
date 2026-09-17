3b59be34ab01f5901364987f7739cf856fcd9ee6
gopirajsathyashouri <gopirajsathyashouri@gmail.com>
2026-09-17 19:41:30 +0530

    Add oops circle

---
+++ oops circle.py
@@ -0,0 +1,13 @@
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+    def circumference(self):
+        return 2 * 3.14 * self.radius
+
+c1 = Circle(5)
+print(c1.area())
+print(c1.circumference())
